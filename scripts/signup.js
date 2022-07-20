308eaffa099a44f1a5c81361ba72248e1da6a6ad
Pratapchandradeo <61131823+Pratapchandradeo@users.noreply.github.com>
2022-07-20 20:17:08 +0530

    signup page

---
+++ scripts/signup.js
@@ -0,0 +1,14 @@
+let from = document.querySelector("form");
+let LSsignupData = JSON.parse(localStorage.getItem("signupData")) || [];
+from.addEventListener("submit",function(event){
+    event.preventDefault();
+    let SignupData = {
+        email:from.email.value,
+        fname:from.Fname.value,
+        lname:from.Lname.value,
+        password:from.password.value
+    }
+    localStorage.setItem("signupData",JSON.stringify(SignupData));
+    alert("SignUp Successful.")
+    console.log(SignupData)
+});
