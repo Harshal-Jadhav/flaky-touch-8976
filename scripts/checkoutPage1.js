a515b5f65433d8c43d3e3d149bb0ac3e028557ce
Harshal-Jadhav <103627954+Harshal-Jadhav@users.noreply.github.com>
2022-07-21 23:48:11 +0530

    Update

--- scripts/checkoutPage1.js
+++ scripts/checkoutPage1.js
@@ -1,10 +1,27 @@
-document.querySelector('#prcheck').addEventListener('click',toggle)
 
-function toggle(){
+// !Function to toggle promocode div
+let toggle = () => {
     let x = document.querySelector('#promocode')
-    if(x.style.display=='flex'){
-        x.style.display='none'
-    }else{
-        x.style.display='flex'
+    if (x.style.display == 'flex') {
+        x.style.display = 'none'
+        document.querySelector('#prcheck>span').innerText = 'add'
+    } else {
+        x.style.display = 'flex'
+        document.querySelector('#prcheck>span').innerText = 'remove'
     }
 }
+document.querySelector('#prcheck').addEventListener('click', toggle)
+
+
+// !Function to toggle Order Products height
+let toggleHeight = () => {
+    let x = document.getElementById('orderProducts');
+    if (x.style.height == '435px') {
+        x.style.height = 'fit-content'
+        document.getElementById('showbtn').innerText = '- Show Less'
+    } else {
+        x.style.height = '435px'
+        document.getElementById('showbtn').innerText = '+ Show more'
+    }
+}
+document.getElementById('showbtn').addEventListener('click', toggleHeight);
